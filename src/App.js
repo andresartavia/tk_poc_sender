aafb7a870edf1ced4aa742fe1bab1d165ac6e693
andres <andres@ip-192-168-12-64.ec2.internal>
2018-03-01 13:44:53 -0600

    starting to add oAuth

--- src/App.js
+++ src/App.js
@@ -1,15 +1,70 @@
 import React, { Component } from 'react';
 import logo from './assets/tk-logo.png';
+import variables from './variables';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.googleIntegration = this.googleIntegration.bind(this);
+  constructor(props) {
+    super(props);
   }
 
-  googleIntegration() {
-    alert("hey!");
+  componentDidMount(){
+    (function() {
+        var e = document.createElement("script");
+        e.type = "text/javascript";
+        e.async = true;
+        e.src = "https://apis.google.com/js/client:platform.js?onload=gPOnLoad";
+        var t = document.getElementsByTagName("script")[0];
+        t.parentNode.insertBefore(e, t)
+    })();    
+  }
+
+  //Triggering login for google
+  googleLogin = () => {
+    let response = null;
+    window.gapi.auth.signIn({
+        callback: function(authResponse) {
+            this.googleSignInCallback( authResponse )
+        }.bind( this ),
+        clientid: variables.google, //Google client Id
+        cookiepolicy: "single_host_origin",
+        requestvisibleactions: "http://schema.org/AddAction",
+        scope: "https://www.googleapis.com/auth/plus.login email"
+    });
+  }
+
+  googleSignInCallback = (e) => {
+    console.log( e )
+    if (e["status"]["signed_in"]) {
+        window.gapi.client.load("plus", "v1", function() {
+            if (e["access_token"]) {
+                this.getUserGoogleProfile( e["access_token"] )
+            } else if (e["error"]) {
+                console.log('Import error', 'Error occured while importing data')
+            }
+        }.bind(this));
+    } else {
+        console.log('Oops... Error occured while importing data')
+    }
+  }
+
+  getUserGoogleProfile = accesstoken => {
+    var e = window.gapi.client.plus.people.get({
+        userId: "me"
+    });
+    e.execute(function(e) {
+        if (e.error) {
+            console.log(e.message);
+            console.log('Import error - Error occured while importing data')
+            return
+        
+        } else if (e.id) {
+            //Profile data
+            alert("Successfull login from google : "+ e.displayName )
+            console.log( e );
+            return;
+        }
+    }.bind(this));
   }
 
   render() {
@@ -20,7 +75,7 @@ class App extends Component {
           <p className="App-intro">
             Welcome to Trendkite's Proof of Concept for a OAuth and Google Analytics Integration.
           </p>
-          <button onClick={() => this.googleIntegration()} />
+          <button onClick={() => this.googleLogin()} />
         </div>
       </div>
     );
---
+++ src/variables.js
@@ -0,0 +1,3 @@
+export default {
+    google  : "917575417096-ui6n7la7ktq26pkgp0mmbcjjg0lg0tk2.apps.googleusercontent.com"
+}
